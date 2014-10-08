887dffeb9fa8c7602d42eea79cfb99e7c7910655
Cody Mace <codymace@gmail.com>
2014-10-08 15:34:18 -0600

    Add working .aar libraries to project

--- clip-in-app-checkout-example/app/src/main/java/com/payclip/clip_in_app_checkout_example/MyActivity.java
+++ clip-in-app-checkout-example/app/src/main/java/com/payclip/clip_in_app_checkout_example/MyActivity.java
@@ -1,17 +1,31 @@
 package com.payclip.clip_in_app_checkout_example;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.payclip.mpl.ClipWidget;
 
-public class MyActivity extends Activity {
+import retrofit.RetrofitError;
+
+
+public class MyActivity extends Activity implements ClipWidget.ClipWidgetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        ClipWidget _widget = new ClipWidget(this, "fJbkv0ipMUi1tPxbzT4AmCpikwzlKt9X2dO2e8Us0U8", "kumar-startbucks1", "ksdafkljdsakjfso3edsfs", "25.00", getResources().getDrawable(R.drawable.clip_icon) , "My App");
+        _widget.setColorScheme(ClipWidget.ClipWidgetColorScheme.CLIP_WIDGET_COLOR_SCHEME_DARK);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.widget_container, _widget.fragment);
+        fragmentTransaction.commit();
     }
 
 
@@ -32,5 +46,31 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**** Clip Widget Listener Methods ****/
+    @Override
+    public void widgetViewReady() {
+
+    }
+
+    @Override
+    public void paymentFinishedWithTransactionId(String s) {
+
+    }
+
+    @Override
+    public void clipWidgetDismissedWithError(Error error) {
+
+    }
+
+    @Override
+    public void clipWidgetPaymentCanceled() {
+
+    }
+
+    @Override
+    public void clipWidgetSetupDidFailWithError(RetrofitError retrofitError) {
+
     }
 }
